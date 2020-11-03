9274beece7d48ec0ded194399d94fb373335f2d2
Ahmed Mrad <mrad.ahmed5@gmail.com>
2020-11-03 18:00:19 +0100

    updates

--- Cleaner.py
+++ Cleaner.py
@@ -1,28 +1,32 @@
 import pandas as pd
-import numpy as np
 import glob
 import os
 import os.path
-import time
 import FTP_Downloader as ftp
 
-os.chdir(os.getcwd())
-path = 'Enter your own path'
-# check if the URL is still working
-url_link = 'https://lehd.ces.census.gov/data/lodes/LODES7'
-state_mapping = pd.read_csv('state.csv')
-all_files = sorted(
-    [os.path.basename(x) for x in glob.glob(os.path.join(path, '*.csv'))])
-# extract specific columns, you might want to choose other columns
-columns_toExtract = [
-    'w_geocode', 'Year', 'C000', 'CNS01', 'CNS02', 'CNS03', 'CNS04', 'CNS05',
-    'CNS06', 'CNS07', 'CNS08', 'CNS09', 'CNS10', 'CNS11', 'CNS12', 'CNS13',
-    'CNS14', 'CNS15', 'CNS16', 'CNS17', 'CNS18', 'CNS19', 'CNS20'
-]
+
+def get_files(path):
+    """Get list of all files with their respective path
+
+    :param path: path of the file to get
+    :type path:
+    :returns all_files: list of all available csv files
+    :rtype all_files: list
+    """
+    all_files = sorted(
+    [os.path.basename(x) for x in glob.glob(os.path.join(path, '*.csv.gz'))])
+    return all_files
 
 
-def state_getter():
-    states = ftp.get_href(url_link, 9)
+def state_getter(url_link):
+    """Create a list of states
+
+    :param url_link: url link to download the available states
+    :type url_link str
+    :returns state_list: list of available states
+    :rtype state_list: list
+    """
+    states = ftp.get_href(url_link, 10)
     state_list = []
     for state in states:
         state_list.append(state.split('/')[0])
@@ -30,38 +34,23 @@ def state_getter():
 
 
 def main():
-    state_list = state_getter()
-    counter = 0
+    current_path = os.getcwd()
+    path_to_files = current_path + '/lehd_files'
+    output_folder = path_to_files + '/concatenated_files'
+    state_list = state_getter('https://lehd.ces.census.gov/data/lodes/LODES7')
+    all_files = get_files(path_to_files)
     for state in state_list:
-        for name in state_mapping['state']:
-            if state == 'us' or state == 'pr' or state == 'vi':
+        if state == 'us' or state == 'pr' or state == 'vi':
                 continue
             else:
-                if state == name.lower():
-                    file_perState = [
-                        file for file in all_files
-                        if state in file.split('_')[0]
-                    ]
-                    list_dframes = [
-                        pd.read_csv(file_name) for file_name in file_perState
-                    ]
-                    for dataframe, file_name in zip(list_dframes,
-                                                    file_perState):
-                        dataframe['Year'] = file_name.split('_')[4].split('.')[
-                            0]
-                    list_dframes = [
-                        pd.DataFrame(file_name, columns=columns_toExtract)
-                        for file_name in list_dframes
-                    ]
-                    concatenated_dataFrame = pd.concat(
-                        list_dframes, ignore_index=True)
+                file_perState = [file for file in all_files if state in file.split('_')[0]]
+                list_dframes = [pd.read_csv(file_name,compression='gzip') for file_name in file_perState]
+                for dataframe, file_name in zip(list_dframes,file_perState):
+                    dataframe['Year'] = file_name.split('_')[4].split('.')[0]
+                    list_dframes = [pd.DataFrame(file_name) for file_name in list_dframes]
+                    concatenated_dataFrame = pd.concat(list_dframes, ignore_index=True)
                     concatenated_dataFrame.sort_values(by=['Year'])
-                    concatenated_dataFrame.to_csv(
-                        'enter your own path/LEHD_%s.csv'
-                        % state_mapping['state_code'][counter],
-                        index=False)
-                    counter += 1
-
+                    concatenated_dataFrame.to_csv('{0}/LEHD_{1}.csv'.format(output_folder,state), index=False)
 
 if __name__ == '__main__':
     main()
--- FTP_Downloader.py
+++ FTP_Downloader.py
@@ -1,26 +1,21 @@
-'''
-ftp DOWNLOADER
-Create an interface that will allow smouth downlowding
-Make it an interface
-#!/apps/anaconda3-4.3.0/bin/python
-#!/apps/anaconda-4.2.0/bin/python
-
-if element is not existing flag it and store it in order to eliminate it
-'''
-#url_link = 'https://lehd.ces.census.gov/data/lodes/LODES7'
-
-import sys
 import os
 import os.path
 import requests
 from bs4 import BeautifulSoup
 import mechanicalsoup as ms
-
-os.chdir(os.getcwd())
-browser = ms.StatefulBrowser()
+import shutil
 
 
 def go_to_next(url, element):
+    '''Extract urls of next pages
+
+    :param url: url link
+    :type url: str
+    :param element: element to be extracted
+    :type element:
+    :returns browser.get_url(): extracted url with mechanical soup
+    :rtype browser.get_url(): str
+    '''
     browser = ms.StatefulBrowser()
     browser.open(url)
     browser.follow_link(element)
@@ -28,6 +23,15 @@ def go_to_next(url, element):
 
 
 def get_href(url, number_elemenent_delete):
+    '''Extract href from website
+
+    :param url: url link
+    :type url: str
+    :param number_elements_delete: number of empty folders
+    :type number_elements_delete: int
+    :returns my_list: list of href links
+    :rtype my_list: list
+    '''
     url_soup = requests.get(url).text
     soup = BeautifulSoup(url_soup, 'html.parser')
     my_list = []
@@ -38,36 +42,51 @@ def get_href(url, number_elemenent_delete):
 
 
 def get_links_toDownload(url):
+    """Extract url links to be downloaded from the main url link
+
+    :param url: url link
+    :type url: str
+    ...
+    :raises LinkNotFoundError: link not working or not found
+    ...
+    :returns download: list url of all the csv files
+    :rtype downlaod: list
+    :returns last_page: urls of the folder where the csv files are located
+    :rtype last_page: list
+    """
     next_page = []
     last_page = []
-    Download = []
-    state_list = get_href(url, 9)
+    download = []
+    # the first 10 href are not state folders. check in the future
+    state_list = get_href(url, 10)
+    print(state_list)
     for state in state_list:
         next_page.append(go_to_next(url, state))
     for page in next_page:
         try:
+            print(page)
             last_page.append(go_to_next(page, 'wac/'))
         except ms.LinkNotFoundError:
             continue
-    for page_toDownload in last_page:
-        Download.append(get_href(page_toDownload, 5))
-    return Download, last_page
+    for page_to_download in last_page:
+        print(page_to_download)
+        download.append(get_href(page_to_download, 5))
+    return download, last_page
 
 
 def main():
-    '''
-    url_link = 'https://lehd.ces.census.gov/data/lodes/LODES7'
-    Check the link because it might have changed
-    '''
-    input_url = sys.argv[1]
-    print(input_url)
-    (Download, last_page) = get_links_toDownload(input_url)
+    current_path = os.getcwd()
+    all_lehd_files_path = current_path + '/lehd_files'
+    if os.path.exists(all_lehd_files_path):
+        shutil.rmtree(all_lehd_files_path)
+    os.makedirs(all_lehd_files_path)
+    os.chdir(all_lehd_files_path)
+    (download, last_page) = get_links_toDownload(
+        'https://lehd.ces.census.gov/data/lodes/LODES7')
     for x in range(len(last_page)):
-        for y in range(len(Download[x])):
-            file_to_download = last_page[x] + Download[x][y]
-            filename = Download[x][y]
-            print(file_to_download)
-            print(filename)
+        for y in range(len(download[x])):
+            file_to_download = last_page[x] + download[x][y]
+            filename = download[x][y]
             r = requests.get(file_to_download)
             with open(filename, 'wb') as f:
                 f.write(r.content)
